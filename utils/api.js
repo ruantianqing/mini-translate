ef2ba332cf2d98b58c970ac9b637e5aeb50b57b7
ruantianqing <1871348727@qq.com>
2021-03-24 20:08:18 +0800

    提交

---
+++ utils/api.js
@@ -0,0 +1,55 @@
+import md5 from './md5.min.js'
+
+const appid = '20210324000740555'
+const key = 'luCqKo_wxI8mfNPnMX8i'
+
+function translate(q, {
+  from = 'auto',
+  to = 'auto'
+} = {
+    from: 'auto',
+    to: 'auto'
+}) {
+  return new Promise((resolve, reject) => {
+    let salt = Date.now()
+    let sign = md5(`${appid}${q}${salt}${key}`)
+    wx.request({
+      url: 'https://fanyi-api.baidu.com/api/trans/vip/translate',
+      data: {
+        q,
+        from,
+        to,
+        appid,
+        salt,
+        sign
+      },
+      success(res) {
+        if (res.data && res.data.trans_result) {
+          resolve(res.data)
+        } else {
+          reject({
+            status: 'error',
+            msg: '翻译失败'
+          })
+          wx.showToast({
+            title: '翻译失败',
+            icon: 'none',
+            duration: 2000
+          })
+        }
+      },
+      fail() {
+        reject({
+          status: 'error',
+          msg: '翻译失败'
+        })
+        wx.showToast({
+          title: '网络异常',
+          icon: 'none',
+          duration: 2000
+        })
+      }
+    })
+  })
+}
+module.exports.translate = translate
